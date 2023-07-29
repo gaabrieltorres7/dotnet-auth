f3f7e0e6438a1cb7f4960b8a93de39de20359137
Gabriel Torres <playerjogador10@outlook.com>
2023-07-29 17:14:02 -0300

    feat: add CreateUserDto

---
+++ Data/Dtos/CreateUserDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace dotnet_auth.Data.Dtos;
+
+public class CreateUserDto
+{
+    [Required] public string Username { get; set; }
+    [Required] public DateTime dateOfBirth { get; set; }
+    [Required] [DataType(DataType.Password)] public string Password { get; set; }
+    [Required] [Compare("Password")] public string RePassword { get; set; }
+}
