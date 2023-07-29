2d51e85d4fb0aff312f1a41c121a20778c90e983
Gabriel Torres <playerjogador10@outlook.com>
2023-07-29 18:26:07 -0300

    refactor: rename prop

--- Data/Dtos/CreateUserDto.cs
+++ Data/Dtos/CreateUserDto.cs
@@ -5,7 +5,7 @@ namespace dotnet_auth.Data.Dtos;
 public class CreateUserDto
 {
     [Required] public string Username { get; set; }
-    [Required] public DateTime dateOfBirth { get; set; }
+    [Required] public DateTime DateOfBirth { get; set; }
     [Required] [DataType(DataType.Password)] public string Password { get; set; }
     [Required] [Compare("Password")] public string RePassword { get; set; }
 }
