82322ba95ad5cd16d51554684d656779c83c8046
Gabriel Torres <playerjogador10@outlook.com>
2023-07-30 11:47:32 -0300

    feat: add scoped registerService

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
 using dotnet_auth.Data;
 using dotnet_auth.Models;
+using dotnet_auth.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
-builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var connectionString = builder.Configuration.GetConnectionString("UserConnection");
 
@@ -20,6 +19,15 @@ builder.Services
     .AddIdentity<User, IdentityRole>()
     .AddEntityFrameworkStores<UserDbContext>()
     .AddDefaultTokenProviders();
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<RegisterService>(); 
+/*
+ AddScoped - chamei uma req nova, vou instanciar um novo
+ AddSigleton - um unico registerService para cada req que chegasse, ou seja, a mesma instancia
+ AddTranslent - sempre uma instancia nova, mesmo que seja na mesma req
+*/
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
