1d61d32599e19b4a9d84149cbc092253cdedfa21
Gabriel Torres <playerjogador10@outlook.com>
2023-07-30 11:52:08 -0300

    refactor: call registerService method

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -1,8 +1,5 @@
-using AutoMapper;
-using dotnet_auth.Data;
 using dotnet_auth.Data.Dtos;
-using dotnet_auth.Models;
-using Microsoft.AspNetCore.Identity;
+using dotnet_auth.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_auth.Controllers;
@@ -11,24 +8,17 @@ namespace dotnet_auth.Controllers;
 [Route("[Controller]")]
 public class UserController : ControllerBase
 {
-    private UserDbContext _context;
-    private IMapper _mapper;
-    private UserManager<User> _userManager;
+    private RegisterService _registerService;
 
-    public UserController(IMapper mapper, UserManager<User> userManager)
+    public UserController(RegisterService registerService)
     {
-        _mapper = mapper;
-        _userManager = userManager;
+        _registerService = registerService;
     }
 
     [HttpPost]
     public async Task<IActionResult> RegisterUser(CreateUserDto userDto)
     {
-        var user = _mapper.Map<User>(userDto);
-        var result = await _userManager.CreateAsync(user, userDto.Password);
-
-        if (result.Succeeded) return Ok("User has been registered");
-
-        throw new ApplicationException("Failed to register user!");
+        await _registerService.Register(userDto);
+        return Ok("User has been registered");
     }
 }
